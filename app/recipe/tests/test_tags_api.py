8bdca5e1c9a543d9d560c4c633cd4c8c454d0e78
echeadle <echeadle@gmail.com>
2021-09-19 15:30:12 -0600

    mispelled filename __init__.py; It would not run tests

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 from django.test import TestCase
 
 from rest_framework import status
-from rest_framework import APIClient
+from rest_framework.test import APIClient
 
-from core.models import Tag, Recipe
+from core.models import Tag
 
 from recipe.serializers import TagSerializer
 
