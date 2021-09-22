29ded93dd2e77af17caf8fd6145b16d7b3d1b386
echeadle <echeadle@gmail.com>
2021-09-21 20:56:43 -0600

    Add list recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -34,3 +34,15 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retireve the recipies for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
