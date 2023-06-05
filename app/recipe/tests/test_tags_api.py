3756b3c71a579ffafd316e2c80cb967e30bdec1e
echeadle <echeadle@gmail.com>
2023-06-05 09:28:46 -0600

    Adding tag views and serializers

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.test import TestCase
 
 from rest_framework import status
-from rest_framework import APIClient
+from rest_framework.test import APIClient
 
 from core.models import Tag
 
