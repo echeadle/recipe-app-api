b31485097e0f65a0189d4e799dfd3564f5d5c7c7
echeadle <echeadle@gmail.com>
2023-06-16 12:36:26 -0600

    adding health check api

--- app/app/settings.py
+++ app/app/settings.py
@@ -150,6 +150,6 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
-SPECTACULAR_SETTINGS ={
+SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
 }
